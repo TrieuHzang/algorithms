b2235e77a3668e860116fd25624fbf3cd872f0ad
Trieu Ha Giang <trieuhagiang1312@gmail.com>
2026-09-03 16:17:46 +0700

    Time: 0 ms (100%), Space: 12.9 MB (76.56%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,14 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int i=0;
+    int j=1;
+    if(numsSize == 0){
+        return 0;
+    }
+    for(j=1; j < numsSize; j++){
+        if(nums[i] != nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i+1;
+}
