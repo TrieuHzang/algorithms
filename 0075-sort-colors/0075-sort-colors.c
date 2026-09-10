69c07d419f0c2234094a9789746d15d7960956de
Trieu Ha Giang <trieuhagiang1312@gmail.com>
2026-09-10 23:56:25 +0700

    Time: 0 ms (100%), Space: 10.4 MB (95.56%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,27 @@
+void sortColors(int* nums, int numsSize) {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 0) {
+            a++;
+        }
+        else if (nums[i] == 1) {
+            b++;
+        }
+        else {
+            c++;
+        }
+    }
+    int index = 0;
+    for (int i = 0; i < a; i++) {
+        nums[index++] = 0;
+    }
+    for (int i = 0; i < b; i++) {
+        nums[index++] = 1;
+    }
+    for (int i = 0; i < c; i++) {
+        nums[index++] = 2;
+    }
+}
