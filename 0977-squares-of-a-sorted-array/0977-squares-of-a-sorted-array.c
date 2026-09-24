a30fb37b7b58f1e997479a3d1ad70b94e7d82ca5
Trieu Ha Giang <trieuhagiang1312@gmail.com>
2026-09-24 23:35:51 +0700

    Time: 0 ms (100%), Space: 25.5 MB (48.62%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.c
@@ -0,0 +1,24 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    int *result = (int *)malloc(numsSize * sizeof(int));
+    int left = 0;
+    int right = numsSize - 1;
+    int k = numsSize - 1;
+    while (left <= right) {
+        int leftSquare = nums[left] * nums[left];
+        int rightSquare = nums[right] * nums[right];
+
+        if (leftSquare > rightSquare) {
+            result[k] = leftSquare;
+            left++;
+        } else {
+            result[k] = rightSquare;
+            right--;
+        }
+        k--;
+    }
+    *returnSize = numsSize;
+    return result;
+}
